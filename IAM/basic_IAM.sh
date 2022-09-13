9d1612380efee8eead2795bded2cbfa86a0363b4
Daniel Feliciano <dani.felic7@gmail.com>
2022-09-12 23:15:09 -0300

    basic iam

---
+++ IAM/basic_IAM.sh
@@ -0,0 +1,10 @@
+# Comandos IAM para Amazon CLI
+
+#Criar perfil
+aws iam create-login-profile --user-name awsS3user --password Training123!
+
+# Listar policies
+aws iam list-policies
+
+#Listar usuários
+aws iam list-users
